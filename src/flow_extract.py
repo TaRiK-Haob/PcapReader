3f77f284ca87221bfceb7f34275a561b1362e573
TaRiK <1160280151@qq.com>
2024-05-13 15:34:35 +0800

    add basic file input

--- src/flow_extract.py
+++ src/flow_extract.py
@@ -1,12 +1,44 @@
 import os
 import sys
 import dpkt
+import datetime
 import socket
-
 
 workspace=sys.path[0]
 
-def main():
+
+def get_flows(input_file):
+	with open(input_file, 'rb') as f:
+		pcap = dpkt.pcap.Reader(f)
+		cnt = 10
+		for timestamp, buf in pcap:
+			print('Timestamp: ', str(datetime.timedelta(timestamp)))
+
+def main(input, output):
+	pcap_filelist = []
+	for root, dirs, files in os.walk(input):
+		for file in files:
+			pcap_filelist.append(os.path.join(root, file))
+	print("共计{}个pcap文件".format(len(pcap_filelist)))
+
+
+	all_flows = []
+	for file in pcap_filelist:
+		try:
+			file_flows = get_flows(file)
+		except Exception as e:
+			print(e)
+			pass
+		if file_flows == False:
+			print(file, "ERROR")
+			continue
+		if len(file_flows) <= 0:
+			continue
+		all_flows += file_flows
+	
+
 
 if __name__ == "__main__":
-    main()
+	#参数：pcap目录路径 csv输出文件
+	_, input_dir, output_file = sys.argv
+	main(input_dir, output_file)
